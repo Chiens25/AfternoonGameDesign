5a4338c4daabfb1990506026f83048c732f3ee67
Sydney Chien <chiens25@greenhill.org>
2022-06-04 01:21:58 -0500

    6/4

--- Age.py/Age.py
+++ Age.py/Age.py
@@ -4,17 +4,17 @@ import os
 os.system('cls')
 
 yearborn = int(input("input the year you were born: "))
-currentyear = 2022
-age = currentyear - yearborn
-print (age)
+currentyear = 2022 
+age = currentyear - yearborn # Subtract year born from current year to find age
+print ("you are", age)
 
-if(age < 50):
+if(age < 50): # Less than statemant 
     print("you are young")
 
-if(age > 50):
+if(age > 50): # Greater than statement
     print("you are old")
 
-if(age == 50):
+if(age == 50): # Equal to statement
     print("you are blah")
 
 
--- IntegersFloatsAssignment.py
+++ IntegersFloatsAssignment.py
@@ -17,17 +17,18 @@ number2 = int(input("input random number:"))
 remainder2 = number2 % 3 #Find remainder of 3
 remainder3 = number2 % 5 #Find remainder of 5
 
-if remainder2 == 0:
+if remainder2 == 0: # If no remainder then multiple of 3
     print("Multiple of 3")
 else:
     print("Not a multiple of 3")
-if remainder3 == 0:
+if remainder3 == 0: # If no remainder then multiple of 5
     print("Multiple of 5")
 else:
     print("Not a multiple of 5")
-if remainder2 == 0 and remainder3 == 0:
+if remainder2 == 0 and remainder3 == 0: # Practice "and" statements
     print("Multiple of both")
 
+# Getting the last digits practice
 print(number2%10) # Get the last digit
 print(number2%100) # Get the last 2 digits
 print(number2%1000) # Get the last 3 digits
