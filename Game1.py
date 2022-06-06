f5f7d6043f942981a25baf234628f248ce3b7deb
Sydney Chien <chiens25@greenhill.org>
2022-06-06 13:34:42 -0500

    6/6

--- Game1.py
+++ Game1.py
@@ -17,45 +17,86 @@ import os
 import random
 os.system('cls')
 
+Game = True
+
 for instructions in range(60): # Top border
     print("+", end = "")
 print()
 
 print("|                     GUESS THE COLOR                      |") # Title
-print("|        The randomizer will choose a random color.        |") # Instructions
+print("| The randomizer will choose a random color/animal/fruit.  |") # Instructions
 print("|         The object of the game is to guess that          |")
 print("|          color in the least amount of chances.           |")
 print("|                        Good Luck!                        |")
-
-colors = ["blue", "pink", "red", "purple", "yellow", "orange"] # Color list
 
 for instructions2 in range(60): # Bottom border
     print("+", end = "")
 print()
 
-print("HINT: The word is one of my FAVORITE COLORS") # Hint
+colors = ["blue", "pink", "red", "purple", "yellow", "orange"] # Color list
+animals = ["lion", "cheetah", "leopard", "puma", "tiger"] # Animal list
+fruits = ["apple", "strawberry", "raspberry", "cherry", "cranberry"] # Fruit list
 
-element = random.choice(colors)
-print(element," is the correct answer (for testing convenience)") # For testing
+choice = input("Which game would you like to play? 1, 2,or 3?: ")
 
-guess = input("Input your guess:").lower() # Make sure the guess is in lower case
+os.system('cls')
 
-if guess == element: # If =
-    print("Congratulations!")
-else: # If !=
-    print("Sorry you missed.")
+if choice == "1":
+    while Game == True:
+        print("HINT: The word is one of my FAVORITE COLORS") # Hint
 
-again = input("Do you want to play again?:").lower()
-# Couldn't figure out how to replay the game 
-    
+        element = random.choice(colors)
+        print(element," is the correct answer (for testing convenience)") # For testing
 
-      
+        guess = input("Input your guess:").lower() # Make sure the guess is in lower case
 
+        if guess == element: # If =
+            print("Congratulations!")
+        else: # If !=
+            print("Sorry, you missed.")
 
+        again = input("Do you want to play again?:").lower()
 
+        if again == "no":
+            Game = False
+            print("Thanks for playing!")
 
+if choice == "2":
+     while Game == True:
+        print("HINT: The word is an animal in the CAT FAMILY") # Hint
 
+        element = random.choice(animals)
+        print(element," is the correct answer (for testing convenience)") # For testing
 
+        guess = input("Input your guess:").lower() # Make sure the guess is in lower case
 
+        if guess == element: # If =
+            print("Congratulations!")
+        else: # If !=
+            print("Sorry, you missed.")
 
+        again = input("Do you want to play again?:").lower()
 
+        if again == "no":
+            Game = False
+            print("Thanks for playing!")
+
+if choice == "3":
+     while Game == True:
+        print("HINT: The word is a RED FRUIT") # Hint
+
+        element = random.choice(fruits)
+        print(element," is the correct answer (for testing convenience)") # For testing
+
+        guess = input("Input your guess:").lower() # Make sure the guess is in lower case
+
+        if guess == element: # If =
+            print("Congratulations!")
+        else: # If !=
+            print("Sorry, you missed.")
+
+        again = input("Do you want to play again?:").lower()
+
+        if again == "no":
+            Game = False
+            print("Thanks for playing!")
