d5c3ea6556489bd9a2f8d562192332746b73848c
Sydney Chien <chiens25@greenhill.org>
2022-06-08 16:25:07 -0500

    6/8

--- Game 2/Game2.py
+++ Game 2/Game2.py
@@ -3,37 +3,37 @@
 
 # *PROBLEM* - WORKS ON THE FIRST RUN AND THEN GOES STRAIGHT TO CHOICE 5
 
-import os, datetime 
-import random
+import os, datetime # Import date and time
+import random # Import random function
 os.system('cls')
 
-high = 0
-
+# Define variables
+high = 0 
+number = 0
 Game = True
 count = 0
 
-
-def scoreboard():
+def hint(): # Define hint
     global count     
-    if count == 0 and choice == 2:
+    if count == 0 and choice == 2: # After first try on 1-25
         remainder = number % 2
-        if remainder == 0: 
+        if remainder == 0: # If remainder is zero/one then it is an even/odd number
                 print("HINT: The number is EVEN")
         else: 
                 print("HINT: The number is ODD")
-    if count == 0 and choice == 3:
+    if count == 0 and choice == 3: # After first try on 1-50
         remainder = number % 2
-        if remainder == 0: 
+        if remainder == 0: # If remainder is zero/one then it is an even/odd number
                 print("HINT: The number is EVEN")
         else: 
                 print("HINT: The number is ODD")
-    if count == 0 and choice == 4:
+    if count == 0 and choice == 4: # After first try on 1-100
         remainder = number % 2
-        if remainder == 0: 
+        if remainder == 0: # If remainder is zero/one then it is an even/odd number
                 print("HINT: The number is EVEN")
         else: 
                 print("HINT: The number is ODD")
-    elif count != 0 and count < 4:
+    elif count == 0 and count < 4: # For guesses 1-5 tells user if guess is too high/low 
         if guess > number:
             print("Too high!")
         if guess < number:
@@ -45,21 +45,21 @@ def scoreboard():
 def randomNumber(choice):  
     global number
     if choice == 2:
-        number = random.randint(1, 25)  
+        number = random.randint(1, 25) # Random number generator
     if choice == 3:
-         number = random.randint(1, 50)
+         number = random.randint(1, 50) # Random number generator
     if choice == 4:
-         number = random.randint(1, 100)
+         number = random.randint(1, 100) # Random number generator
     return number  
 
-name = input("What is your name?: ")
-high = 0 
+name = input("What is your name?: ") # Input name for scoreboard
+high = 0 # Define high
 
-while Game:
+while Game: 
     for menu in range(56): # Top border
         print("+", end = "")
     print()
-    print("|                    NUMBER GAME                       |") # Title
+    print("|                    NUMBER GAME                       |") # Main menu
     print("|                 1. Instructions                      |") 
     print("|                 2. Play Level 1-25                   |")
     print("|                 3. Play Level 1-50                   |")
@@ -70,10 +70,43 @@ while Game:
     for menu in range(56): # Bottom border
         print("+", end = "")
     print()
-    choice = int(input("Select option 1-6: "))
+    choice =input("Select option 1-6: ") # User input option choice
+    while True:
+        try: # Accounts for non 1-7 inputs
+            choice=int(choice) # Make it an integer so you dont have to go through with "" plus it adds value
+            if choice>0 and choice <7: 
+                break
+            else:
+                print("give me 1-7")
+        except:
+            print("sorry")
+    number = randomNumber(choice) # Set number to random number
+    if choice == 1: # Print the instructions
+        myFile = open("G2.txt", 'r') 
+        content = myFile.readlines()
+        for line in content:
+            print(line)
+        myFile.close()
 
-    number = randomNumber(choice) 
-    os.system('cls')
+    if choice == 5: # Print the scoresheet
+        myFile = open("G2score.txt", 'r')
+        stuff = myFile.readlines()
+        myFile.close()
+        stuff.sort(reverse = True) # Puts largest digit on the top
+        for line in stuff:
+            print(line)
+
+    if choice == 6:
+        
+        print("Thanks for playing!", high)
+        if high > 50:
+            myFile = open("Game 2\G2score.txt", 'a')
+            date=datetime.datetime.now()
+            scrLine = str(high)+"\t "+ name + "\t"+ date.strftime("%m-%d-%Y")+ "\n"
+            myFile.write(scrLine)
+            myFile.close()
+        break
+        os.system('cls')
     check = True
     while check and count <5:
         guess = int(input("Plese put your guess here: "))
@@ -82,34 +115,18 @@ while Game:
             print("Congrats, You got it!")
             check=False
         else:
-            scoreboard()
+            hint()
         count+=1   
         if count ==5:
             print("Sorry! The number was", number )
-    score = 200-40*count
+    score = 800-40*count
     if score > high:   
         high=score
     print(name +", your score is "+str(score))
     input("Press enter: ")
     os.system('cls')
+print(high)
 
-if choice == 1:
-    myFile = open("G2.txt", 'r')
-    content = myFile.readlines()
-    for line in content:
-        print(line)
-    myFile.close()
-
-if choice == 5:
-    myFile = open("G2score.txt", 'r')
-    stuff = myFile.readlines()
-    myFile.close()
-    stuff.sort(reverse = True)
-    for line in stuff:
-        print(line)
-
-if choice == 6:
-    print("Thanks for playing!")
 
 
 
