520f3de0a18c749b8c1416dab277b858114517ef
Sydney Chien <chiens25@greenhill.org>
2022-06-10 13:49:02 -0500

    6/10

--- PygameFile.py/FirstPygame.py
+++ PygameFile.py/FirstPygame.py
@@ -1,7 +1,9 @@
 # Sydney Chien
 # We are going to learn basic pygame functions
  # Find colors: Search "rgb color chart"
-import pygame, os, time
+from locale import CHAR_MAX
+from turtle import circle
+import pygame, os, random, math, time
 pygame.init()
 os.system('cls')
 
@@ -19,28 +21,98 @@ pygame.time.delay(3000)
 blueClr = (0, 0, 255)
 redClr = (255, 0, 0)
 cyanClr = (0, 255, 177)
+
+# images
+bg = pygame.image.load("PygameFile.py\Images\\bgSmaller.jpg")
+char = pygame.image.load("PygameFile.py\Images\PixelArtTutorial.png")
+char = pygame.transform.scale(char, (50,50))
 # screen.fill(blueClr)
 # pygame.display.update() # Have to do after any change that the user can see
 # pygame.time.delay(3000)
+
+# Var for square
 hb = 50 # Height value
 wb = 50
 xb = 325 # Where it will be displayed
 yb = 325
-square = (xb, yb, hb, wb)
+square = pygame.Rect(xb, yb, hb, wb)
 
+charx = xb
+chary = yb
+
+# Var for circle
+cx = 350
+cy = 350
+rad = 25
+ibox = rad*math.sqrt(2)
+xig = cx-(ibox/2)
+yig = cy-(ibox/2)
+
+speed = 1
 run = True
 background = clr
 while run:
-    screen.fill(background)
+    screen.blit(bg, (0,0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             print("you quit")
+        # Mouse position coordinates
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mousePos = pygame.mouse.get_pos()
+            print(mousePos)
+# Square movement
+    keys = pygame.key.get_pressed() # provide a list of 11 keys
+    if keys [pygame.K_a] and square.x > speed:
+        square.x -= speed
+    if keys[pygame.K_d] and square.x < WIDTH - (wb+speed):
+        square.x += speed
+    if keys [pygame.K_w] and square.y > speed:
+        square.y -= speed
+    if keys[pygame.K_d] and square.y < HEIGHT - (hb+speed):
+        square.y += speed
+# Circle movement
+    if keys [pygame.K_LEFT] and cx > (speed+rad):
+        cx -= speed
+        inSquare.x -= speed
+    if keys[pygame.K_RIGHT] and cx < WIDTH - (rad+speed):
+        cx += speed
+        inSquare.x += speed
+    if keys [pygame.K_UP] and cy > (rad+speed):
+        cy -= speed
+        inSquare.y -= speed
+    if keys[pygame.K_DOWN] and cy < HEIGHT - (rad+speed):
+        cy += speed
+        inSquare.y += speed
+
     # rect(surface, color, object)
+    if square.collidepoint((cx,cy)):
+        print("BOOM")
+        cx = random.randint(rad,WIDTH-rad)
+        cy = random.randint(rad,HEIGHT-rad)
+        rad+= 5
+        # Inscibed square
+        ibox = rad*math.sqrt(2)
+        xig = cx-(ibox/2)
+        yig = cy-(ibox/2)
+        inSquare = pygame.Rect(xig,yig,ibox, ibox)
+        screen.blit(char, (200,200))
+
+    # Mountain collid
+    if square.colliderect(mountainSquare):
+        square.x = 10
+        square.y = 10
+        charx = 10
+        chary = 10
+        
+        #Bounce
+        mountainSquare = pygame.Rect(200,270,180,250)
+        pygame.draw.rect(screen, colors.get("white"), mountainSquare)
+        
     pygame.draw.rect(screen, blueClr, square)
     # circle(surface, color, center, radius)
-    pygame.draw.circle(screen, cyanClr, (350, 350), 25)
+    pygame.draw.circle(screen, cyanClr, (cx, cy), rad) # Circle is not an object
     pygame.display.update()
-
+    pygame.time.delay(5)
    
     
