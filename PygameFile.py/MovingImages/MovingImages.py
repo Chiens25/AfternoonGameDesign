4c244dc34f40b52e757104474f3fe10855d9c44f
Sydney Chien <chiens25@greenhill.org>
2022-06-13 13:31:00 -0500

    6/13

--- PygameFile.py/MovingImages/MovingImages.py
+++ PygameFile.py/MovingImages/MovingImages.py
@@ -1,17 +1,24 @@
+# Sydney Chien
+# Importing images to use in the game
+# Learning how to move characters
+
 import pygame
 pygame.init()
 
-win = pygame.display.set_mode((500,480))
+win = pygame.display.set_mode((500,480)) # Windown size
 
-pygame.display.set_caption("First Game")
+pygame.display.set_caption("First Game") # Name Game 
 
+# Provided by Youtube Guy
+# REMEMBER TO COPY RELATIVE PATH
 walkRight = [pygame.image.load('PygameFile.py\MovingImages\R1.png'), pygame.image.load('PygameFile.py\MovingImages\R2.png'), pygame.image.load('PygameFile.py\MovingImages\R3.png'), pygame.image.load('PygameFile.py\MovingImages\R4.png'), pygame.image.load('PygameFile.py\MovingImages\R5.png'), pygame.image.load('PygameFile.py\MovingImages\R6.png'), pygame.image.load('PygameFile.py\MovingImages\R7.png'), pygame.image.load('PygameFile.py\MovingImages\R8.png'), pygame.image.load('PygameFile.py\MovingImages\R9.png')]
 walkLeft = [pygame.image.load('PygameFile.py\MovingImages\L1.png'), pygame.image.load('PygameFile.py\MovingImages\L2.png'), pygame.image.load('PygameFile.py\MovingImages\L3.png'), pygame.image.load('PygameFile.py\MovingImages\L4.png'), pygame.image.load('PygameFile.py\MovingImages\L5.png'), pygame.image.load('PygameFile.py\MovingImages\L6.png'), pygame.image.load('PygameFile.py\MovingImages\L7.png'), pygame.image.load('PygameFile.py\MovingImages\L8.png'), pygame.image.load('PygameFile.py\MovingImages\L9.png')]
-bg = pygame.image.load('PygameFile.py\MovingImages\\bg.jpg')
-char = pygame.image.load('PygameFile.py\MovingImages\standing.png')
+bg = pygame.image.load('PygameFile.py\MovingImages\\bg.jpg') # Background image
+char = pygame.image.load('PygameFile.py\MovingImages\standing.png') # Character image for standing
 
 clock = pygame.time.Clock()
 
+# Setting all used variables
 x = 50
 y = 400
 width = 64
@@ -26,7 +33,7 @@ walkCount = 0
 
 def redrawGameWindow():
     global walkCount
-    win.blit(bg, (0,0))
+    win.blit(bg, (0,0)) # Blit makes things appear on the game window
 
     if walkCount + 1 >= 27:
         walkCount = 0
@@ -48,26 +55,28 @@ run = True
 while run:
     clock.tick(27)
 
-    for event in pygame.event.get():
+    for event in pygame.event.get(): # Allow for quit
         if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and x > vel:
+    # Move left only if not at the left border
+    if keys[pygame.K_LEFT] and x > vel:# Left arrow key
         x -= vel
         left = True
         right = False
+    # Same for right
     elif keys[pygame.K_RIGHT] and x < 500 - width - vel:
         x += vel
         right = True
         left = False
-    else:
+    else: # If not, stay in place
         right = False
         left = False
         walkCount = 0
         
-    if not(isJump):
+    if not(isJump): # Space key to use jump
         if keys[pygame.K_SPACE]:
             isJump = True
             right = False
